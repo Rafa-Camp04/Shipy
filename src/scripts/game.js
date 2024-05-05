03a54432240787949a33e51eb36b40d756a44c5c
Rafael Campos <rafaelcampos04@hotmail.com>
2024-05-05 15:20:56 -0400

    background test

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -13,7 +13,8 @@ class Game extends MovingObject {
 
         // this.backgroundImg.src = '../media/orange-test.webp';
         // this.backgroundImg.src = '../media/background_02.jpeg';
-        this.backgroundImg.src = '../../media/background_01.jpeg';
+        // this.backgroundImg.src = '../../media/background_01.jpeg';
+        this.backgroundImg.src = '\\wsl.localhost\Ubuntu-20.04\home\rafacamp04\aa-projects\Shipy\Shipy\media\background_01.jpeg';
     };
 
     addEnemies() {
