a60fc20529eb2e06a6e9b8e17e781071638c3b5f
Rafael Campos <rafaelcampos04@hotmail.com>
2024-05-06 10:25:17 -0400

    background testing

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -14,7 +14,7 @@ class Game extends MovingObject {
 
         // this.backgroundImg.src = '../media/orange-test.webp';
         // this.backgroundImg.src = '../media/background_02.jpeg';
-        // this.backgroundImg.src = '../media/background_01.jpg';
+        this.backgroundImg.src = '../media/background_01.jpg';
     };
 
     // runs the game
@@ -39,6 +39,7 @@ class Game extends MovingObject {
 
     // spawn new enemies
     addEnemies() {
+        debugger
         for (let index = 0; this.enemies.length < 8; index++) {
             const enemy = new Enemy({});
             this.enemies.push(enemy);
